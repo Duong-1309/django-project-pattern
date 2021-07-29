7a22321d964edde9ece9f4d0c3157761505b4eb7
Duong-1309 <vanduongk1309@gmail.com>
2021-07-29 10:06:44 +0700

    Update file .env and settings project

--- app/django_pattern/settings.py
+++ app/django_pattern/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+dotenv_path = Path('/home/vanduong/data/projects_learning/django-project-pattern/.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o(!1%+c$jb*)xol@tp@umi%glvpo!ji5&@1%l1kc$5$xho#9g-'
+# SECRET_KEY = 'django-insecure-o(!1%+c$jb*)xol@tp@umi%glvpo!ji5&@1%l1kc$5$xho#9g-'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
